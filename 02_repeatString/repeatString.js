6ba7444346c671e1dd1c74d2f0c5dd4f57a88480
zippy0626 <zippy0626@gmail.com>
2024-09-03 11:49:48 -0400

    Solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let newString = "";
 
+    if (num < 0) {
+        return "ERROR"
+    }
+
+    for (let i = 0; i <= num - 1; i++) {
+        newString += string
+    };
+
+    return newString
 };
 
 // Do not edit below this line
