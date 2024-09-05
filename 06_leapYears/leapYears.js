64c22fcf0e72d60277b4a2374fc9e42e5e87b6ad
zippy0626 <zippy0626@gmail.com>
2024-09-04 20:49:01 -0400

    solved leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 4 === 0) {
+        
+        // !year % 100 === 0 is wrong
+        if (year % 100 != 0) {
+            return true
+        } else if (year % 100 === 0 && year % 400 === 0) {
+            return true
+        } else {
+            return false
+        };
 
+    } else {
+        return false
+    };
 };
 
 // Do not edit below this line
