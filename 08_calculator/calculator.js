49f6566384e03ce34788e5c3ee7817eb9b584e5a
zippy0626 <zippy0626@gmail.com>
2024-09-13 11:05:43 -0400

    Solved Calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a ,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	if (array.length > 0) { // the sum() was not returning a value
+    return array.reduce((total, currentNum) => {
+      return total + currentNum; // this is in it's own scope
+    }, 0)
+  } else {
+    return 0;
+  };
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array
+    .reduce((total, num) => {
+      return total * num
+    }, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    let sum = 1;
+
+    for (let i = 1; i <= num; i++) {
+      sum *= i
+    };
+
+    return sum
+  };
 };
 
 // Do not edit below this line
