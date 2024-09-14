bebb835897ea7b49fc9880419dae0a83ca8a3612
zippy0626 <zippy0626@gmail.com>
2024-09-13 20:29:50 -0400

    Solved fibs

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if (n < 0) {
+    return "OOPS";
+  }
+  n = parseInt(n);
+  return Math.round(
+    (((1 + Math.sqrt(5)) / 2) ** n - ((1 - Math.sqrt(5)) / 2) ** n) /
+      Math.sqrt(5)
+  );
 };
 
 // Do not edit below this line
