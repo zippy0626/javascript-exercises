5ec44657c4b9e21d4ca5660990e2492cccfde291
zippy0626 <zippy0626@gmail.com>
2024-09-14 21:55:25 -0400

    Solved findtheoldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const oldestYear = people
+    .map((person) => {
+      if (!person.hasOwnProperty('yearOfDeath')) { // if yearOfDeath is missing,
+        const date = new Date()
+        let year = date.getFullYear()
 
+        person['yearOfDeath'] = year
+      }
+      return person.yearOfDeath - person.yearOfBirth
+    })
+    .reduce((leftNum, rightNum) => {
+      if (rightNum > leftNum) { // Checks for max number, returns the max number
+        return rightNum 
+      }
+      return leftNum
+    }, 0)
+
+  return people.find((person) => { // DONT FORGET return 
+    return (person.yearOfDeath - person.yearOfBirth)===oldestYear
+  })
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
